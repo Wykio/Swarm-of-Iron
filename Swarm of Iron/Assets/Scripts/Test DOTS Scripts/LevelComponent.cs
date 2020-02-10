e7be5e6414849271c1506913f5a3ef3367f90bd1
Antoine <antoine.thenevin@gmail.com>
2020-02-10 16:08:57 +0100

    Import Job System package and Burst Compiler package

---
+++ Swarm of Iron/Assets/Scripts/Test DOTS Scripts/LevelComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+// components are always struct
+public struct LevelComponent : IComponentData
+{
+    public float level;
+}
