6af99c26af27af06de2bd9dada03688df9e47b9c
Antoine <antoine.thenevin@gmail.com>
2020-03-03 15:36:19 +0100

    upgrade to Unity 2019.3.0f6

--- Swarm of Iron/Assets/Test/LevelComponent.cs
+++ Swarm of Iron/Assets/Test/LevelComponent.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 
-// components are always struct
-public struct LevelComponent : IComponentData
-{
+public struct LevelComponent : IComponentData {
     public float level;
 }
---
+++ Swarm of Iron/Assets/Test/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Testing : MonoBehaviour {
+    public void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        Entity entity = entityManager.CreateEntity(typeof(LevelComponent));
+
+        entityManager.SetComponentData(entity, new LevelComponent { level = 10 });
+    }
+
+}
