00e44deedb7e5a3130516822ccf1ef2b9fac9445
Antoine <antoine.thenevin@gmail.com>
2020-03-03 20:24:21 +0100

    ECS works with 3D objects !!!

---
+++ Swarm of Iron/Assets/TestECS/Scripts/Systems/LevelUpSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class LevelUpSystem : ComponentSystem {
+
+    protected override void OnUpdate() {
+        Entities.ForEach((ref LevelComponent levelcomponent) => {
+            //Increment le level à chaque frame
+            levelcomponent.level += 1.0f * Time.DeltaTime;
+        });
+        //throw new System.NotImplementedException();
+    }
+}
---
+++ Swarm of Iron/Assets/TestECS/Scripts/Systems/MoverSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoverSystem : ComponentSystem {
+
+    protected override void OnUpdate() {
+        Entities.ForEach((ref Translation translation, ref MoveSpeedComponent moveSpeedComponent) => {
+            //Increment la translation à chaque frame
+            translation.Value.y += moveSpeedComponent.moveSpeed * Time.DeltaTime;
+            if (translation.Value.y > 5.0f) {
+                moveSpeedComponent.moveSpeed = -math.abs(moveSpeedComponent.moveSpeed);
+            }
+            if (translation.Value.y < -5.0f) {
+                moveSpeedComponent.moveSpeed = +math.abs(moveSpeedComponent.moveSpeed);
+            }
+        });
+        //throw new System.NotImplementedException();
+    }
+}
