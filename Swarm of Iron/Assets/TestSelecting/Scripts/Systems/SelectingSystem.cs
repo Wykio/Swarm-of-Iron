e4e4baa984d92e428fc0666e0a6b0824ccfb301d
florianvazelle <ponythugflorian@gmail.com>
2020-03-04 11:40:50 +0100

    Right way to select PlayerUnitSelect

--- Swarm of Iron/Assets/TestSelecting/Scripts/Systems/SelectingSystem.cs
+++ Swarm of Iron/Assets/TestSelecting/Scripts/Systems/SelectingSystem.cs
@@ -9,15 +9,17 @@ public class SelectingSystem : ComponentSystem {
 
     protected override void OnUpdate() {
         // Action is play when entity is selecting
-        Entities.ForEach((ref PlayerUnitSelect select, ref Translation translation, ref AABB aabb) => {
-            // When update position
-            translation.Value.y += 2.0f * Time.DeltaTime;
+        Entities
+            .WithAll<PlayerUnitSelect>()
+            .ForEach((ref Translation translation, ref AABB aabb) => {
+                // When update position
+                translation.Value.y += 2.0f * Time.DeltaTime;
 
-            // you should update Hitbox
-            aabb = new AABB {
-                max = translation.Value + 0.5f,
-                min = translation.Value - 0.5f,
-            };
-        });
+                // you should update Hitbox
+                aabb = new AABB {
+                    max = translation.Value + 0.5f,
+                    min = translation.Value - 0.5f,
+                };
+            });
     }
 }
