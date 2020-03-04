21b7616f9c857b0854e8fc9d723f49daf7f6787b
Antoine <antoine.thenevin@gmail.com>
2020-03-04 23:51:26 +0100

    refactoring + Start gameProject with Unit Selection by throwing raycast

---
+++ Swarm of Iron/Assets/Scripts/Swarm_Of_Iron.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+
+namespace Swarm_Of_Iron_namespace
+{
+    public class Swarm_Of_Iron : MonoBehaviour
+    {
+        private EntityManager entityManager;
+
+        [SerializeField] int spawnUnitAmount;
+        [SerializeField] private Mesh soldierMesh;
+        [SerializeField] private Material soldierMaterial;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            for (int i = 0; i < spawnUnitAmount; i++)
+            {
+                SpawnSoldier();
+            }
+        }
+
+        private void SpawnSoldier()
+        {
+            SpawnSoldier(new float3(UnityEngine.Random.Range(-10.0f, 10.0f), 1.0f, UnityEngine.Random.Range(-10.0f, 10.0f)));
+        }
+
+        private void SpawnSoldier(float3 spawnPosition)
+        {
+            EntityArchetype entityArchetype = entityManager.CreateArchetype(
+                typeof(Soldier),
+                typeof(MoveTo),
+                typeof(Translation),
+                typeof(LocalToWorld),
+                typeof(RenderMesh),
+                typeof(RenderBounds)
+            );
+
+            Entity entity = entityManager.CreateEntity(entityArchetype);
+
+            entityManager.SetComponentData(entity, new Translation { Value = spawnPosition });
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = soldierMesh,
+                material = soldierMaterial
+            });
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 clickPosition = -Vector3.one;
+
+                //Method 1
+                //clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0.0f , 0.0f, 10.0f));
+
+                //Method 2
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit)) {
+                    clickPosition = hit.point;
+                }
+                //Goal
+                Debug.Log(clickPosition);
+            }
+        }
+    }
+}
---
+++ Swarm of Iron/Assets/Scripts/Systems/UnitControlSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+namespace Swarm_Of_Iron_namespace
+{
+    public class UnitControlSystem : ComponentSystem
+    {
+        private float3 startPosition;
+
+        protected override void OnUpdate()
+        {
+            if (Input.GetMouseButtonDown(0)) {
+                // Mouse Pressed
+                //startPosition = 
+            }
+            if (Input.GetMouseButtonUp(0)) {
+                // Mouse Released
+            }
+
+        }
+    }
+}
