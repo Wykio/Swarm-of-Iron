d6154d0952b0f62b1fd037e55e2ef74aa46b52c9
Antoine <antoine.thenevin@gmail.com>
2020-03-06 10:12:07 +0100

    Starting to group units

--- Swarm of Iron/Assets/Scripts/Systems/UnitControlSystem.cs
+++ Swarm of Iron/Assets/Scripts/Systems/UnitControlSystem.cs
@@ -99,10 +99,15 @@ namespace Swarm_Of_Iron_namespace
 
             // right click
             if (Input.GetMouseButtonDown(1)) {
+                //move unit
+                float3 targetPosition = getMousePosition();
+                List<float3> movePositionList = GetPositionListAround(targetPosition, 3.0f, 5);
+                //List<float3> movePositionList = GetPositionListAround(targetPosition, new float[] { 2.0f, 4.0f, 6.0f }, new int[] { 5, 10, 20 });
+                int positionIndex = 0;
                 Entities.WithAll<UnitSelected>().ForEach((Entity entity, ref MoveTo moveTo) => {
-                    moveTo.position = getMousePosition();
+                    moveTo.position = movePositionList[positionIndex];
+                    positionIndex = (positionIndex + 1) % movePositionList.Count;
                     moveTo.move = true;
-                    //Debug.Log("move to" + moveTo.position);
                 });
             }
         }
@@ -119,6 +124,45 @@ namespace Swarm_Of_Iron_namespace
                 return -Vector3.one;
                 Debug.Log("Click on Nothing !");
             }
+        }
+
+        private List<float3> GetPositionListAround(float3 centerPosition, float[] ringDistance, int[] ringPositionCount)
+        {
+            List<float3> positionList = new List<float3>();
+            positionList.Add(centerPosition);
+            for (int ring = 0; ring < ringPositionCount.Length; ring++) {
+                List<float3> ringPositionList = GetPositionListAround(centerPosition, ringDistance[ring], ringPositionCount[ring]);
+                positionList.AddRange(ringPositionList);
+            }
+            return positionList;
+        }
+
+        // Create positions for Units to not overlap
+        private List<float3> GetPositionListAround(float3 startPosition, float distance, int positionCount)
+        {
+            List<float3> positionList = new List<float3>();
+            positionList.Add(startPosition);
+            positionList.Add(startPosition + new float3(distance, 0.0f, 0.0f));
+            positionList.Add(startPosition + new float3(-distance, 0.0f, 0.0f));
+            positionList.Add(startPosition + new float3(0.0f, 0.0f, distance));
+            positionList.Add(startPosition + new float3(0.0f, 0.0f, -distance));
+            /*
+            for (int i = 0; i < positionCount; i++) {
+                int angle = i * (360 / positionCount);
+                float3 direction = ApplyRotationToVector(new float3(1.0f, 0.0f, 0.0f), angle);
+                float3 position = startPosition + direction * distance;
+                positionList.Add(position);
+            }
+            */
+            for (int i = 0; i < positionCount; i++) {
+                Debug.Log(positionList[i]);
+            }
+            return positionList;
+        }
+
+        // take a vector and apply an angle to it
+        private float3 ApplyRotationToVector(float3 vector, float angle) {
+            return Quaternion.Euler(0, 0, angle) * vector;
         }
     }
 }
