967702f1db1e21911625ac054990f3e72ce86fb7
Antoine <antoine.thenevin@gmail.com>
2020-03-14 16:10:35 +0100

    add comments and refactoring

--- Swarm of Iron/Assets/Scripts/Systems/UnitControlSystem.cs
+++ Swarm of Iron/Assets/Scripts/Systems/UnitControlSystem.cs
@@ -21,7 +21,7 @@ namespace Swarm_Of_Iron_namespace
 
         protected override void OnUpdate()
         {
-            // left click
+            // left click Down
             if (Input.GetMouseButtonDown(0)) {
                 // Mouse Pressed
                 Swarm_Of_Iron.instance.selectionAreaTransform.gameObject.SetActive(true);
@@ -29,96 +29,89 @@ namespace Swarm_Of_Iron_namespace
                 startPositionScreen = Input.mousePosition; //Screen Position
                 Swarm_Of_Iron.instance.selectionAreaTransform.position = startPositionScreen;
             }
-
+            // Hold left click Down
             if (Input.GetMouseButton(0)) {
                 // Mouse Held Down
                 float3 currentPositionScreen = Input.mousePosition;//Screen Position
-                float3 selectionAeraSize = currentPositionScreen - startPositionScreen;
-                //Debug.Log(startPositionScreen + " -> " + currentPositionScreen);
+                float3 selectionAeraSize = currentPositionScreen - startPositionScreen; //Resize SCREEN selection Area
                 Swarm_Of_Iron.instance.selectionAreaTransform.localScale = selectionAeraSize;
-                //Debug.Log(selectionAeraSize);
             }
 
+            // left click Up
             if (Input.GetMouseButtonUp(0)) {
                 // Mouse Released
-                Swarm_Of_Iron.instance.selectionAreaTransform.gameObject.SetActive(false);
-                float3 endPosition = getMousePosition();
-
-                float3 lowerLeftPosition = new float3(math.min(startPosition.x, endPosition.x), 0.0f, math.min(startPosition.z, endPosition.z));
-                float3 upperRightPosition = new float3(math.max(startPosition.x, endPosition.x), 0.0f, math.max(startPosition.z, endPosition.z));
-
-                //Select OneUnitOnly
-                bool selectOnlyOneEntity = false;
-
-                //Click on OneUnitOnly
-                float selectionAreaMinSize = 10.0f;
-                float selectionAreaSize = math.distance(lowerLeftPosition, upperRightPosition);
-                if (selectionAreaSize < selectionAreaMinSize) {
-                    // SelectionArea too small
-                    lowerLeftPosition += new float3(-1, 0, -1) * 0.2f * (selectionAreaMinSize - selectionAreaSize);
-                    upperRightPosition += new float3(+1, 0, +1) * 0.2f * (selectionAreaMinSize - selectionAreaSize);
-                    selectOnlyOneEntity = true;
-                }
-
-                // Deselect all Units
-                Entities.WithAll<UnitSelected>().ForEach((Entity entity) => {
-                    PostUpdateCommands.RemoveComponent<UnitSelected>(entity);
-                });
-                Entities.WithAll<SelectionMeshComponent>().ForEach((Entity entity) => {
-                    Swarm_Of_Iron.instance.entityManager.DestroyEntity(entity);
-                });
-
-                // Select Units
-                int selectedEntityCount = 0;
-                Entities.WithAll<SoldierComponent>().ForEach((Entity entity, ref Translation translation) => {
-                    if (selectOnlyOneEntity == false || selectedEntityCount < 1) {
-                        float3 entityPosition = translation.Value;
-
-                        if (entityPosition.x >= lowerLeftPosition.x &&
-                            entityPosition.z >= lowerLeftPosition.z &&
-                            entityPosition.x <= upperRightPosition.x &&
-                            entityPosition.z <= upperRightPosition.z)
-                        {
-                            //Entity inside selection area
-                            PostUpdateCommands.AddComponent(entity, new UnitSelected());
-                            selectedEntityCount++;
-                            AddEntitySelectionMesh(entity);
-                        }
-                    }
-                });
+                GetAllUnitsInSelectionArea();
             }
 
             // right click
             if (Input.GetMouseButtonDown(1)) {
-                //move unit
-                float3 targetPosition = getMousePosition();
-                //List<float3> movePositionList = GetPositionListAround(targetPosition, 3.0f, 8);
-                
-                //List<float3> movePositionList = GetPositionListAround(targetPosition, new float[] { 3.0f, 5.0f, 7.0f }, new int[] { 8, 12, 16 });
-                //List<float3> movePositionList = GetPositionListAround(targetPosition, Soldier.ringDistancesArray, Soldier.unitsPerRingArray);
-                int positionIndex = 0;
-                Entities.WithAll<UnitSelected>().ForEach((Entity entity, ref MoveToComponent moveTo) => {
-                    moveTo.move = true;
-                    moveTo.position = Soldier.movePositionList[positionIndex] + targetPosition;
-                    positionIndex = (positionIndex + 1) % Soldier.movePositionList.Count;
-                });
+                //move selected units
+                moveAllUnitSelected();
             }
         }
 
-        private float3 getMousePosition()
+        private void GetAllUnitsInSelectionArea()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            Swarm_Of_Iron.instance.selectionAreaTransform.gameObject.SetActive(false); //Desactivate SCREEN Selection Area
+            float3 endPosition = getMousePosition();
+            // Calculate WORLD selection area
+            float3 lowerLeftPosition = new float3(math.min(startPosition.x, endPosition.x), 0.0f, math.min(startPosition.z, endPosition.z));
+            float3 upperRightPosition = new float3(math.max(startPosition.x, endPosition.x), 0.0f, math.max(startPosition.z, endPosition.z));
 
-            if (Physics.Raycast(ray, out hit)) {
-                return hit.point;
+            //Select OneUnitOnly
+            bool selectOnlyOneEntity = false;
+
+            //Click on OneUnitOnly
+            float selectionAreaMinSize = 10.0f;
+            float selectionAreaSize = math.distance(lowerLeftPosition, upperRightPosition);
+            if (selectionAreaSize < selectionAreaMinSize)
+            {
+                // SelectionArea is too small => select only one unit
+                lowerLeftPosition += new float3(-1, 0, -1) * 0.2f * (selectionAreaMinSize - selectionAreaSize);
+                upperRightPosition += new float3(+1, 0, +1) * 0.2f * (selectionAreaMinSize - selectionAreaSize);
+                selectOnlyOneEntity = true;
             }
-            else {
-                return -Vector3.one;
-                Debug.Log("Click on Nothing !");
-            }
+
+            // Deselect all Units and Destroy all entities of the selection Mesh
+            Entities.WithAll<UnitSelected>().ForEach((Entity entity) => {
+                PostUpdateCommands.RemoveComponent<UnitSelected>(entity);
+            });
+            Entities.WithAll<SelectionMeshComponent>().ForEach((Entity entity) => {
+                Swarm_Of_Iron.instance.entityManager.DestroyEntity(entity);
+            });
+
+            // Add "UnitSelected" component and create the entity for the selection Mesh
+            int selectedEntityCount = 0;
+            Entities.WithAll<SoldierComponent>().ForEach((Entity entity, ref Translation translation) => {
+                if (selectOnlyOneEntity == false || selectedEntityCount < 1)
+                {
+                    float3 entityPosition = translation.Value;
+
+                    if (entityPosition.x >= lowerLeftPosition.x &&
+                        entityPosition.z >= lowerLeftPosition.z &&
+                        entityPosition.x <= upperRightPosition.x &&
+                        entityPosition.z <= upperRightPosition.z)
+                    {
+                        //Entity inside selection area
+                        PostUpdateCommands.AddComponent(entity, new UnitSelected());
+                        selectedEntityCount++;
+                        AddEntitySelectionMesh(entity);
+                    }
+                }
+            });
         }
 
+        private void moveAllUnitSelected()
+        {
+            float3 targetPosition = getMousePosition();
+            int positionIndex = 0;
+            Entities.WithAll<UnitSelected>().ForEach((Entity entity, ref MoveToComponent moveTo) => {
+                moveTo.move = true;
+                moveTo.position = Soldier.movePositionList[positionIndex] + targetPosition;
+                positionIndex = (positionIndex + 1) % Soldier.movePositionList.Count;
+            });
+        }
+        
         private void AddEntitySelectionMesh(Entity entityParent)
         {
             EntityManager entityManager = Swarm_Of_Iron.instance.entityManager;
@@ -141,6 +134,22 @@ namespace Swarm_Of_Iron_namespace
                 mesh = Swarm_Of_Iron.instance.unitSelectedCircleMesh,
                 material = Swarm_Of_Iron.instance.unitSelectedCircleMaterial
             });
+        }
+
+        private float3 getMousePosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                return hit.point;
+            }
+            else
+            {
+                return -Vector3.one;
+                Debug.Log("Click on Nothing !");
+            }
         }
     }
 }
