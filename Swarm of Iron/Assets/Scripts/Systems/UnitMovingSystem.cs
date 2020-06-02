9eebdab492ded6818ada02a883d4dd50a702ed6a
florian <florian@harfanglab.fr>
2020-06-02 14:06:26 +0200

    parallelize MiniMap

--- Swarm of Iron/Assets/Scripts/Systems/UnitMovingSystem.cs
+++ Swarm of Iron/Assets/Scripts/Systems/UnitMovingSystem.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Entities;
+﻿using Unity.Entities;
 using Unity.Jobs;
+using Unity.Burst;
 using Unity.Transforms;
 using Unity.Mathematics;
 
@@ -10,32 +8,49 @@ namespace Swarm_Of_Iron_namespace
 {
     public class UnitMovingSystem : JobComponentSystem
     {
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        [BurstCompile]
+        struct MovingJob : IJobForEach<Translation, MoveToComponent>
         {
-            float deltatime = Time.DeltaTime;
-
-            return Entities.ForEach((ref Translation translation, ref MoveToComponent moveTo) => {
-                if (moveTo.move) {
+            public float DeltaTime;
+            public void Execute(ref Translation translation, ref MoveToComponent moveTo)
+            {
+                if (moveTo.move)
+                {
                     float reachedPositionDistance = 1.0f;
 
                     // Far from target position, Move to position
                     float3 moveDir = math.normalize(moveTo.position - translation.Value);
                     moveTo.lastMoveDir = moveDir;
-                    if (moveDir[0] == 0 && moveDir[1] == -1 && moveDir[2] == 0) {
-                        if (moveTo.harvest) {
+                    if (moveDir[0] == 0 && moveDir[1] == -1 && moveDir[2] == 0)
+                    {
+                        if (moveTo.harvest)
+                        {
                             float3 hub = new float3(0, 0, 0);
                             if (moveTo.position[0] == hub[0] && moveTo.position[1] == hub[1] && moveTo.position[2] == hub[2]) moveTo.position = moveTo.targetPosition;
                             else moveTo.position = hub;
-                        } else {
+                        }
+                        else
+                        {
                             // Already there
                             moveTo.move = false;
                         }
-                    } else {
-                      translation.Value.x += moveDir.x * moveTo.moveSpeed * deltatime;
-                      translation.Value.z += moveDir.z * moveTo.moveSpeed * deltatime;
+                    }
+                    else
+                    {
+                        translation.Value.x += moveDir.x * moveTo.moveSpeed * DeltaTime;
+                        translation.Value.z += moveDir.z * moveTo.moveSpeed * DeltaTime;
                     }
                 }
-            }).Schedule(inputDeps);
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            var job = new MovingJob()
+            {
+                DeltaTime = UnityEngine.Time.deltaTime
+            };
+            return job.Schedule(this, inputDependencies);
         }
     }
 }
