20c7a6e59a30d583f4aadca35ad4865f29158174
Shen <s_lin97@hotmail.com>
2020-06-20 22:52:41 +0200

    AI ChaseTarget + StopAt_atkPosition

--- Swarm of Iron/Assets/Scripts/Systems/EnemieMoveSystem.cs
+++ Swarm of Iron/Assets/Scripts/Systems/EnemieMoveSystem.cs
@@ -13,11 +13,13 @@ namespace SOI
     public class EnemieMoveSystem : ComponentSystem
     {
         private EntityQuery EnemiQuery, UnitQuery;
+        private EndSimulationEntityCommandBufferSystem endSimulationEntityCommandBufferSystem;
 
         [BurstCompile]
         struct FindTarget : IJobForEachWithEntity<Translation>
         {
             [ReadOnly] public NativeArray<Translation> U_positions;
+            public EntityCommandBuffer.Concurrent entityCommandBuffer;
             //public NativeArray<Translation> E_positions;
             
 
@@ -27,9 +29,20 @@ namespace SOI
 
                 for (int i = 0; i < U_positions.Length; i++)
                 {
-                    if (math.distance(position, U_positions[i].Value) < 5f)
+                    if (math.distance(position, U_positions[i].Value) < 30f)
                     {
-                        translation.Value = U_positions[i].Value+1;
+                        entityCommandBuffer.AddComponent(idxEntity, entity, new MoveToComponent
+                        {
+                            startPosition = translation.Value,
+                            endPosition = U_positions[i].Value+1
+                        });
+
+                        if (math.distance(position, U_positions[i].Value) < 18f)
+                        {
+                            entityCommandBuffer.RemoveComponent<MoveToComponent>(idxEntity, entity);
+                        }
+
+                        //translation.Value = U_positions[i].Value+1;
                     }
                 }
             }
@@ -39,6 +52,7 @@ namespace SOI
         {
             EnemiQuery = GetEntityQuery(ComponentType.ReadOnly<E_UnitComponent>(), ComponentType.ReadWrite<Translation>());
             UnitQuery = GetEntityQuery(ComponentType.ReadOnly<UnitComponent>(), ComponentType.ReadOnly<Translation>());
+            endSimulationEntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
         }
 
         protected override void OnUpdate()
@@ -49,10 +63,12 @@ namespace SOI
             var job = new FindTarget()
             {
                 U_positions = AllUnitPos,
+                entityCommandBuffer = endSimulationEntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent(),
                 //E_positions = AllEnemiPos
             };
             JobHandle dependency = job.Schedule(EnemiQuery);
 
+            endSimulationEntityCommandBufferSystem.AddJobHandleForProducer(dependency);
             dependency.Complete();
             // AllEnemiPos.Dispose(dependency);
             AllUnitPos.Dispose(dependency);
